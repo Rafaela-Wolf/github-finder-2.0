5728cfa1e0a3bc3ee298fffb8ba82b6f0e5e7c93
Rafaela Wolf <rafaelawolf52@gmail.com>
2025-02-28 15:23:01 -0300

    resgatando usuário da API do github e colocando estilos inicias no search e app

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
+import classes from './Search.module.css';
 
-const Search = () => {
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>
+}
+
+const Search = ({loadUser}: SearchProps) => {
+
+  const [userName, setUserName] = useState("");
+
   return (
-    <div>
+    <div className={classes.search}>
         <h2>Busque por um usuário:</h2>
         <p>Conheça seus melhores repositórios</p>
-        <div>
+        <div className={classes.search_container}>
             <input 
                 type="text"
                 placeholder="Digite o nome do usuário"
+                onChange={(e) => setUserName(e.target.value)}
             />
-            <button><BsSearch /></button>
+            <button onClick={() => loadUser(userName)}><BsSearch /></button>
         </div>
     </div>
   )
